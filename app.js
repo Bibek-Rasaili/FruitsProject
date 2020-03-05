36f832e806504ef8e4b7db2be3a4b269ce66821e
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-03-05 20:03:02 +0000

    Connection Successful

---
+++ app.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = 'fruitsDB';
+
+
+// Create a new MongoClient
+const client = new MongoClient(url); // , { useNewUrlParser: true }
+
+
+// Use connect method to connect to the Server
+client.connect(function(err){
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  // const collection = client.db("test").collection("devices");
+
+  // perform actions on the collection object
+  client.close();
+});
